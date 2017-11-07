ffcf2c61f9b66328f226632ff0241c48d71afb8b
Hanna Walgrave <Walgrave.hanna@hotmail.com>
2017-11-07 18:55:49 +0100

    installed dusk

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        return view('home');
+        if (Auth::check())
+        {
+            $user = Auth::user();
+            return view('home.index', compact('user'));
+        }
+
+        return view('users.login');
     }
 }
